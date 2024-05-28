1c8ebe62a1cf24e4f18d00a3374b4dfa28d7200f
DanrevLazo <161433786+Danrev29@users.noreply.github.com>
2024-05-28 17:57:53 +0800

    Add files via upload

---
+++ website.js
@@ -0,0 +1,49 @@
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 150;
+
+        if (revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add('active');
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+function toggleDescription(descriptionId) {
+    var description = document.getElementById(descriptionId);
+    description.classList.toggle("active");
+}
+function toggleImageSize(imageId) {
+    var image = document.getElementById(imageId);
+    image.classList.toggle("enlarged");
+}
+
+let preveiwContainer = document.querySelector('.products-preview');
+let previewBox = preveiwContainer.querySelectorAll('.preview');
+
+document.querySelectorAll('.products-container .product').forEach(product =>{
+  product.onclick = () =>{
+    preveiwContainer.style.display = 'flex';
+    let name = product.getAttribute('data-name');
+    previewBox.forEach(preview =>{
+      let target = preview.getAttribute('data-target');
+      if(name == target){
+        preview.classList.add('active');
+      }
+    });
+  };
+});
+
+previewBox.forEach(close =>{
+  close.querySelector('.fa-times').onclick = () =>{
+    close.classList.remove('active');
+    preveiwContainer.style.display = 'none';
+  };
+});
+
+
